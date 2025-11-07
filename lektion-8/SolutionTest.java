2afec2268400004e75c9a31a8f7f9fcfacf60712
evref <isak.lefevre@hotmail.se>
2025-11-07 10:18:20 +0100

    fix: byt till assertEquals i advanced lists test

--- lektion-8/SolutionTest.java
+++ lektion-8/SolutionTest.java
@@ -18,7 +18,7 @@ public class SolutionTest {
 
 
         assertTrue(Solution.function(list1) == 1);
-        assertTrue(Solution.function(list2) == 1); // Men även tester kan ha buggar! :)
+        assertTrue(Solution.function(list2) == 1); // Men även tester kan ha buggar!
         assertTrue(Solution.function(list3) == 2);
     }
 
@@ -27,8 +27,8 @@ public class SolutionTest {
         List<Integer> list1 = Arrays.asList(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5); 
         List<Integer> list2 = Arrays.asList(2, 6, -20, 0, 8);         
 
-        assertTrue(Solution.function(list1) == 3);
-        assertTrue(Solution.function(list2) == 0);
+        assertEquals(3, Solution.function(list1));
+        assertEquals(0, Solution.function(list2));
     }
 
     @Test
