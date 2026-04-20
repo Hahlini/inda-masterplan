30e1174209b9d4bb0c9c81cb42bd1717311b9f29
Johanhah <Johanhah@kth.se>
2026-04-20 19:52:03 +0200

    Open Source

---
+++ lektion-23/OpenSourceCalculator.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class OpenSourceCalculator {
+    
+
+    /**
+     * @author Johan
+     */
+    static int add (int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * @author Isak
+     */
+    static int average (List<Integer> list) {
+        return list.stream().reduce(0, (a,b) -> add(a, b))/list.size();
+    }
+}
