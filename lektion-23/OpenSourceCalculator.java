5d7312a8de3561757a33f4402bde6c6c9f9efa8c
Isak Lefèvre <isak@lefevre.nu>
2026-04-24 13:53:18 +0200

    Merge remote changes (frfr)

--- lektion-23/OpenSourceCalculator.java
+++ lektion-23/OpenSourceCalculator.java
@@ -16,7 +16,6 @@ public class OpenSourceCalculator {
     static int average (List<Integer> list) {
         return list.stream().reduce(0, (a,b) -> add(a, b))/list.size();
     }
-<<<<<<< HEAD
 
     /**
      * @author Johan
@@ -25,7 +24,6 @@ public class OpenSourceCalculator {
         return a - b;
     }
 
-=======
     
     /**
      * @author Isak
@@ -33,5 +31,4 @@ public class OpenSourceCalculator {
     static int multiply(int a, int b) {
         return a * b;
     }
->>>>>>> bc1e466 (Add multiply to calculator)
 }
